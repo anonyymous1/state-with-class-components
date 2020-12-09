7437dff99160416923ed54bdcc94882a50daaf36
Ruben Cedeno <rubencedeno@rubens-mbp.myfiosgateway.com>
2020-12-09 11:39:45 -0500

    [solution]Add solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state ={
+      count: 0,
+      inputtedNumber: 0
+    }
+    // this.increment = this.increment.bind(this)
+  }
+  
+  increment = () => {
+    const newCount = this.state.count + this.state.inputtedNumber
+    this.setState({count: newCount})
+      // let currentCount = this.state.count
+      // let newCount = currentCount + 1
+      // this.setState({count: newCount})
+  }
+
+  decrement = () => {
+    const newCount = this.state.count - this.state.inputtedNumber
+    this.setState({count: newCount})
+    // let currentCount = this.state.count
+    // let newCount = currentCount - 1
+    // this.setState({count: newCount})
+  }
+
+  changeInput = (evt) => {
+    console.log(evt.target.value);
+    const newValue = parseInt(evt.target.value) || 0
+
+    this.setState({inputtedNumber:newValue})
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>{ this.state.count }</h1>
+        <input type="number" value={this.state.inputtedNumber} onChange={this.changeInput}/>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.decrement}>-</button>
+      </div>
+    )
+  }
 }
+
 
 export default App;
